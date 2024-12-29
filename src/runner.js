9727bc3f849492d077cc8a59dfcd5a82c91d8e28
Niar <depfaris@gmail.com>
2024-12-29 08:39:38 +0700

    Updated runner.js to support running all YAML tests in the tests folder

--- src/runner.js
+++ src/runner.js
@@ -1,4 +1,3 @@
-// src/runner.js
 const { runTest, loadTestCases } = require('./executor');
 const yaml = require('js-yaml');
 const fs = require('fs');
@@ -7,12 +6,20 @@ const path = require('path');
 // Membaca konfigurasi umum dari file YAML
 const config = yaml.load(fs.readFileSync(path.join(__dirname, '../config/test-config.yaml'), 'utf8'));
 
-// Membaca test case dari file YAML
-const testCase = loadTestCases(path.join(__dirname, '../tests/login-test.yaml'));
+// Membaca semua file dengan ekstensi .yaml dari folder tests
+const testFolder = path.join(__dirname, '../tests');
+const testFiles = fs.readdirSync(testFolder).filter(file => file.endsWith('.yaml'));
 
-// Menjalankan pengujian
-runTest(testCase, config).catch((err) => {
-  console.error('Test failed', err);
-});
-
-
+// Menjalankan semua test case
+(async () => {
+  for (const file of testFiles) {
+    try {
+      console.log(`\nRunning test file: ${file}`);
+      const testCase = loadTestCases(path.join(testFolder, file));
+      await runTest(testCase, config);
+      console.log(`Test file ${file} executed successfully.\n`);
+    } catch (err) {
+      console.error(`Test file ${file} failed`, err);
+    }
+  }
+})();
